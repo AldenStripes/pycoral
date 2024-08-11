5b77054422e44b03f89986cca42e87e105518f11
Alden Wang <114105823+AldenStripes@users.noreply.github.com>
2024-08-11 15:37:02 -0400

    Update classify_image.py

--- classify_image.py
+++ classify_image.py
@@ -1,34 +1,3 @@
-# Lint as: python3
-# Copyright 2019 Google LLC
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     https://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-r"""Example using PyCoral to classify a given image using an Edge TPU.
-
-To run this code, you must attach an Edge TPU attached to the host and
-install the Edge TPU runtime (`libedgetpu.so`) and `tflite_runtime`. For
-device setup instructions, see coral.ai/docs/setup.
-
-Example usage:
-```
-bash examples/install_requirements.sh classify_image.py
-
-python3 examples/classify_image.py \
-  --model test_data/mobilenet_v2_1.0_224_inat_bird_quant_edgetpu.tflite  \
-  --labels test_data/inat_bird_labels.txt \
-  --input test_data/parrot.jpg
-```
-"""
-
 import argparse
 import time
 
@@ -102,19 +71,20 @@ def main():
     common.set_input(interpreter, normalized_input.astype(np.uint8))
 
   # Run inference
-  print('----INFERENCE TIME----')
-  print('Note: The first inference on Edge TPU is slow because it includes',
-        'loading the model into Edge TPU memory.')
-  for _ in range(args.count):
-    start = time.perf_counter()
-    interpreter.invoke()
-    inference_time = time.perf_counter() - start
-    classes = classify.get_classes(interpreter, args.top_k, args.threshold)
-    print('%.1fms' % (inference_time * 1000))
-
-  print('-------RESULTS--------')
-  for c in classes:
-    print('%s: %.5f' % (labels.get(c.id, c.id), c.score))
+  with open('inference_results.txt', 'w') as f:
+    print('----INFERENCE TIME----', file=f)
+    print('Note: The first inference on Edge TPU is slow because it includes',
+          'loading the model into Edge TPU memory.', file=f)
+    for _ in range(args.count):
+      start = time.perf_counter()
+      interpreter.invoke()
+      inference_time = time.perf_counter() - start
+      classes = classify.get_classes(interpreter, args.top_k, args.threshold)
+      print('%.1fms' % (inference_time * 1000), file=f)
+  
+    print('-------RESULTS--------', file=f)
+    for c in classes:
+      print('%s: %.5f' % (labels.get(c.id, c.id), c.score), file=f)
 
 
 if __name__ == '__main__':
